1cdc793447701c6a836c0e6c7ca6e14b7dec4ede
Debbl <me@aiwan.run>
2023-10-30 23:28:04 +0800

    feat: use pwa and update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import UnoCSS from "unocss/vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), UnoCSS()],
+  plugins: [
+    react(),
+    UnoCSS(),
+    VitePWA({
+      registerType: "autoUpdate",
+    }),
+  ],
   resolve: {
     alias: {
       "~": "/src",
